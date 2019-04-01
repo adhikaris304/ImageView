cbde97eee05e8a146ee097511bb0460290dbb3fb
Suman Adhikari <adhikaris304@gmail.com>
2019-04-01 10:44:05 +0545

    Image added in drawable

--- app/src/main/java/com/example/imageview/MainActivity.java
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.imageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioButton rd1,rd2,rd3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
